011d16846f65ffc0a073117d94e7ebc741ba2e77
masudattouteki15 <52403447+masudattouteki15@users.noreply.github.com>
2019-09-26 18:41:28 +0900

    Add files via upload

---
+++ homepage/index.js
@@ -0,0 +1,10 @@
+function goto_page1(){
+    window.location.href = "page1.html";
+}
+
+function goto_page2(){
+    window.location.href = "page2.html";
+}
+function goto_page3(){
+    window.location.href = "page3.html";
+}
